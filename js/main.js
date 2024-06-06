d3a3f46f394e23d8b204e9f122c6478790efed24
Пользователь <otabeksediyorov298@gmail.com>
2024-06-07 03:42:00 +0500

    .

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// {
+//     prompt("agar berilgan son musbat bo'lsa  ga oshirilsin yoqsa ozi qaytsin")
+//     let a=+prompt("son kiriting")
+//     if(a>0){
+//     alert (a+1)
+//     }else{
+//     alert (a)
+//     }
+// }
+// {
+//     prompt("agar berilgan son musbat bo'lsa  1 ga oshirilsin yoqsa 2ga kamayib qaytsin")
+//     let a=+prompt("son kiriting")
+//     if(a>0){
+//     alert (a+1)
+//     }else{
+//     alert (a-2)
+//     }
+// }
+// {
+//     prompt("agar berilgan son musbat bo'lsa  1 ga oshirilsin yoqsa 2ga kamayib qaytsin agar 0ga teng bo'lsa 10nni ozlashtrsin")
+//     let a=+prompt("son kiriting")
+//     if(a>0){
+//     alert (a+1)
+//     }else if (a==0){
+//     alert (a+10)
+//     }else{
+//         alert(a-2)
+//     }
+// }
+{
+    prompt("3 ta butun son berilgab shulardan nechtasi musbat")
+    let a=+prompt("son kiriting")
+    let b=+prompt("son kiriting")
+    let c=+prompt("son kiriting")
+    let son=0
+    if(a>0){
+     (son+1)
+    }else if (b>0){
+    (son+1)
+    }else if (c>0){
+        alert(son+1)
+    }else{
+        alert(
+            son
+        )
+    }
+}
